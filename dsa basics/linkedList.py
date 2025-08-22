30cd0af585740b0ba531bb21bad97372181abd9c
Jaden Goh <JGOH164@e.ntu.edu.sg>
2025-08-22 23:40:37 +0800

    single linked lists done

--- dsa basics/linkedList.py
+++ dsa basics/linkedList.py
@@ -5,16 +5,48 @@ class ListNode:
     
     def __repr__(self):
         return f"Node: {self}, Value: {self.val}"
+    
+def displayNodes(head):
+    curr = head
+    while curr:
+        print(curr.val)
+        curr = curr.next
+    print(None)
+
+def searchNode(head, index):
+    if not head:
+        print(None)
+    else:
+        curr = head
+        for i in range(index):
+            if not curr:
+                print(None)
+                break
+            else:
+                curr = curr.next
+        print(curr.val)
+
+def addNode(head,node,index):
+    curr = head
+    if not head:
+        return None
+    elif not node:
+        return None
+    else:
+        for i in range(index-1):
+            if not curr:
+                print(None)
+                break
+            else:
+                curr = curr.next
+        tail = curr.next
+        curr.next = node
+        node.next = tail
+    
 
 colour1 = ListNode("Blue")
 colour2 = ListNode("Red")  
 colour3 = ListNode("Green")
-
-colour1.next = colour2
-colour2.next = colour3
-
-print(colour1.next.next.val)
-
 
 num1 = ListNode(1)
 num2= ListNode(2)
@@ -33,3 +65,9 @@ while cur.next:
     cur = cur.next
 print(cur.val) # Prints 1 2 4 (3 deleted)
  
+displayNodes(num1)
+
+searchNode(num1, 1)
+
+addNode(num1, colour1, 2)
+displayNodes(num1)
