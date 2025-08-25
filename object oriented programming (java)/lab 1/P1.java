47559b6826989ffefa18b33c3a64581fdcf8191c
Jaden Goh <JGOH164@e.ntu.edu.sg>
2025-08-25 22:35:48 +0800

    tuned lab answers

--- object oriented programming (java)/lab 1/P1.java
+++ object oriented programming (java)/lab 1/P1.java
@@ -4,25 +4,16 @@ public class P1 {
     public static void main(String[] args) {
         System.out.print("Enter a Character:");
         Scanner scanner = new Scanner(System.in); // scanner is your variable where it takes in an input such as in Python: name = input("Enter your name: ") 
-        String input = scanner.next();
+        String input = scanner.next().toLowerCase();
 
         switch (input) {
             case "a":
                 System.out.println("Action movie fan\n");
                 break;
-            case "A":
-                System.out.println("Action movie fan\n");
-                break;
             case "c":
                 System.out.println("Comedy movie fan\n");
                 break;
-            case "C":
-                System.out.println("Comedy movie fan\n");
-                break;
             case "d":
-                System.out.println("Drama movie fan\n");
-                break;
-            case "D":
                 System.out.println("Drama movie fan\n");
                 break;
             default:
