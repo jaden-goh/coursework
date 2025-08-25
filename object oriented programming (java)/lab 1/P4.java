a15ada0bf2b099ce04623eae98bece9b2747fa11
#GOH JUN WEI# <JGOH164@student.main.ntu.edu.sg>
2025-08-25 15:08:51 +0800

    p4done

--- object oriented programming (java)/lab 1/P4.java
+++ object oriented programming (java)/lab 1/P4.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class P4 {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in); 
 
-    
+        System.out.print("Enter your Height: ");
+        int height = scanner.nextInt();
+
+        for (int i = 1; i <= height; i++) {
+            int j = 0;
+            String output = "";
+            while (j < i) {
+                if (j % 2 == 0) {
+                    output = output + "AA";
+                }
+                else {
+                    output = output + "BB";
+                }
+                j += 1;
+            }
+            System.out.println(output);
+        }
+    }
 }
