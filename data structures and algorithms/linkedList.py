54e553f1b5809f11ce06334d2d347cb8ff4a533d
Jaden Goh <JGOH164@e.ntu.edu.sg>
2025-08-29 17:20:44 +0800

    updated linked list

--- data structures and algorithms/linkedList.py
+++ data structures and algorithms/linkedList.py
@@ -50,7 +50,10 @@ def searchNode(list, index):
                 break
             else:
                 curr = curr.next
-        print(curr.val)
+        if not curr:
+            print("Index out of range")
+        else:
+            print(curr.val)
 
 def addNode(list,node,index):
     curr = list.head
@@ -89,7 +92,9 @@ def deleteNode(list, index):
                 break
             else:
                 curr = curr.next
-        if not curr.next:
+        if not curr:
+            print("Index out of range")
+        elif not curr.next:
             print("Index out of range")
         else:
             curr.next = curr.next.next
@@ -97,3 +102,66 @@ def deleteNode(list, index):
     
 
 
+# -----------------------------
+# TEST CASES
+# -----------------------------
+
+print("\n--- Test 1: Empty List ---")
+ll = LinkedList()
+displayNodes(ll)
+searchNode(ll, 0)
+
+print("\n--- Test 2: Add at Head ---")
+ll = LinkedList()
+addNode(ll, ListNode(10), 0)
+displayNodes(ll)   # Expected: 10 -> None
+addNode(ll, ListNode(20), 0)
+displayNodes(ll)   # Expected: 20 -> 10 -> None
+
+print("\n--- Test 3: Add in Middle ---")
+ll = LinkedList()
+addNode(ll, ListNode(1), 0)
+addNode(ll, ListNode(3), 1)
+addNode(ll, ListNode(2), 1)
+displayNodes(ll)   # Expected: 1 -> 2 -> 3 -> None
+
+print("\n--- Test 4: Add at Tail ---")
+ll = LinkedList()
+addNode(ll, ListNode(1), 0)
+addNode(ll, ListNode(2), 1)
+addNode(ll, ListNode(3), 2)
+displayNodes(ll)   # Expected: 1 -> 2 -> 3 -> None
+
+print("\n--- Test 5: Delete Head ---")
+ll = LinkedList()
+addNode(ll, ListNode(1), 0)
+addNode(ll, ListNode(2), 1)
+deleteNode(ll, 0)
+displayNodes(ll)   # Expected: 2 -> None
+
+print("\n--- Test 6: Delete Middle ---")
+ll = LinkedList()
+addNode(ll, ListNode(1), 0)
+addNode(ll, ListNode(2), 1)
+addNode(ll, ListNode(3), 2)
+deleteNode(ll, 1)
+displayNodes(ll)   # Expected: 1 -> 3 -> None
+
+print("\n--- Test 7: Delete Tail ---")
+ll = LinkedList()
+addNode(ll, ListNode(1), 0)
+addNode(ll, ListNode(2), 1)
+addNode(ll, ListNode(3), 2)
+deleteNode(ll, 2)
+displayNodes(ll)   # Expected: 1 -> 2 -> None
+
+print("\n--- Test 8: Delete Out of Range ---")
+ll = LinkedList()
+addNode(ll, ListNode(1), 0)
+deleteNode(ll, 5)  # Should print "Index out of range"
+displayNodes(ll)
+
+print("\n--- Test 9: Search Out of Range ---")
+ll = LinkedList()
+addNode(ll, ListNode(1), 0)
+searchNode(ll, 5)  # Expected: None
