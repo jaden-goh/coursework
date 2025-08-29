260e78d6e321cfc7a4cca017c30bb6a3e686580e
Jaden Goh <JGOH164@e.ntu.edu.sg>
2025-08-29 18:25:52 +0800

    ll tut q1 done

---
+++ data structures and algorithms/tutorials/linkedList1.py
@@ -0,0 +1,108 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+    
+    def __repr__(self):
+        return f"Value({self.val})"
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    # 1. Display all nodes
+    def display(self):
+        curr = self.head
+        while curr:
+            print(curr.val, end=" -> ")
+            curr = curr.next
+        print("None")
+
+    # 2. Find node at index
+    def findAt(self, index):
+        if index < 0 or index >= self.size:
+            print("Index out of range")
+            return None
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next
+        return curr
+
+    # 3. Insert node at index
+    def insertNode(self, index, val):
+        if index < 0 or index > self.size:
+            print("Index out of range")
+            return
+
+        new_node = ListNode(val)
+
+        # insert at head
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            prev = self.findAt(index - 1)
+            new_node.next = prev.next
+            prev.next = new_node
+
+        self.size += 1
+
+    # 4. Remove node at index
+    def removeNode(self, index):
+        if self.head is None:
+            print("List is empty")
+            return
+        if index < 0 or index >= self.size:
+            print("Index out of range")
+            return
+
+        if index == 0:
+            to_delete = self.head
+            self.head = self.head.next
+        else:
+            prev = self.findAt(index - 1)
+            to_delete = prev.next
+            prev.next = to_delete.next
+
+        to_delete.next = None
+        self.size -= 1
+        return to_delete.val
+
+    # 5. Get current size
+    def getSize(self):
+        return self.size
+
+
+def move_even_items_to_back(ll: LinkedList):
+    curr = ll.head
+    if not curr:
+        return ll
+    else:
+        evens = LinkedList()
+        count = 0
+        evenCount = 0
+        while curr:
+            if curr.val % 2 == 0:
+                evens.insertNode(evenCount, curr.val)
+                evenCount += 1
+                curr = curr.next
+                ll.removeNode(count)
+            else:
+                curr = curr.next
+                count += 1
+    newcurr = ll.head
+    for i in range(ll.getSize() - 1):
+        newcurr = newcurr.next
+    newcurr.next = evens.head
+
+    return ll
+
+ll = LinkedList()
+for x in [1, 3, 5]:
+    ll.insertNode(ll.getSize(), x)
+print("Before:"); ll.display()
+move_even_items_to_back(ll)
+print("After:"); ll.display()
+# Expected: odds [1, 3, 5] followed by evens [2, 4, 6]
+
