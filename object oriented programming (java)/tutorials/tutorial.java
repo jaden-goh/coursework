fa61f4e78fd87324b22f352e40fbe8a8a1de37fa
Jaden Goh <JGOH164@e.ntu.edu.sg>
2025-09-01 00:11:38 +0800

    oop tutorial

---
+++ object oriented programming (java)/tutorials/tutorial.java
@@ -0,0 +1,15 @@
+public class tutorial {
+    public static void main (String args[]) {
+
+    /* Q1: Student -> Object, NTU -> Object, Book -> Class, MichaelJackson -> Object, Age -> Attribute, Color -> Attribute, Work -> Behavior, Person -> Class, Person1 -> Object, Result -> Attribute, Transformer -> Object, Engine -> Attribute, Liquid -> Attribute, Force -> Attribute, Shoot -> Behavior 
+    *  Q2: School
+            -> Department {name, head of department, courses} 
+            -> Courses {course code, difficulty, }
+            -> Teacher {PhD, Courses Taught, Office Number, Teacher ID}
+            -> Student {major, courses taken, grade level, id}
+            -> Rooms {hardware (bool), category (leisure, tutorial, lecture), courses supported}
+            
+    * 
+    */
+    }
+}
