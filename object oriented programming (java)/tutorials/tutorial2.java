14349228c53362f20aa12fbdd98149a677471c46
Jaden Goh <JGOH164@e.ntu.edu.sg>
2025-09-08 11:22:41 +0800

    oop tutorial 2

---
+++ object oriented programming (java)/tutorials/tutorial2.java
@@ -0,0 +1,64 @@
+class Circle {
+        private double radius; // radius
+        private static final double pi = 3.14159;
+
+        public Circle(double rad) {
+            this.radius = rad;
+        
+        }
+        
+        public void setRadius(double rad) {
+            this.radius = rad;
+        }
+        
+        public double getRadius() {
+            return this.radius;
+        }
+
+        public double area() {
+            double area = Circle.pi * this.radius * this.radius;
+            return area;
+        }
+
+        public double circumference() {
+            double circ = Circle.pi * (2*this.radius);
+            return circ;
+        }
+
+        public void printArea() {
+            double area = this.area();
+            System.out.println(area);
+        }
+        public void printCircumference() {
+            double circ = this.circumference();
+            System.out.println(circ);
+        }
+    }
+
+class Dice {
+        private int valueOfDice;
+
+        public Dice(int val) {
+            this.valueOfDice = val;
+        }
+        
+        public void setDiceValue(int val) {
+            this.valueOfDice = val;
+        }
+
+        public int getDiceValue(int val) {
+            return this.valueOfDice;
+        }
+
+        public void printDiceValue() {
+            int val = this.valueOfDice;
+            System.out.println(val);
+        }
+    }
+
+public class tutorial2 {
+    
+    
+}
+
+
