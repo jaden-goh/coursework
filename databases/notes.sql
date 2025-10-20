b59bc745ea2587477ed14c006644797090116acc
Jaden Goh <JGOH164@e.ntu.edu.sg>
2025-10-20 23:35:42 +0800

    database

--- databases/notes.sql
+++ databases/notes.sql
@@ -12,3 +12,22 @@ VALUES ('shoes', 5, 65), ('pants', 5, 30);
 -- view entire table
 SELECT *
 FROM product;
+
+-- view certain columns
+SELECT prodnames
+FROM product;
+
+-- view rows of certain columns under certain conditions
+SELECT prodnames
+FROM product
+WHERE prodprice > 45;
+
+
+-- update values (on condition)
+UPDATE product
+SET prodprice = prodprice - 10
+WHERE prodprice > 40;
+
+-- delete values (on condition)
+DELETE FROM product
+WHERE prodnames = 'wrong';
